d4e93bb0abf1e287e9a7833416e3ac73f7917c94
kumarsudarshan <ksudarsh@in.ibm.com>
2021-06-20 13:03:09 +0530

    kumar_ branch_ message

--- src/serialization/SerializeDemo.java
+++ src/serialization/SerializeDemo.java
@@ -3,5 +3,6 @@ package serialization;
 public class SerializeDemo {
     public static void main(String[] args){
         System.out.println("hello github");
+        System.out.println("Hello this is kumar_1 branch");
     }
 }
